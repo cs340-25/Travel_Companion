389342521afb53d773329edd1fb83d3fb6ca3983
Hippoblumbus <jhenley9@vols.utk.edu>
2025-03-30 09:27:52 -0400

    Add Xcode project with initial view and map image

---
+++ Travel Companion/Travel Companion/Travel_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_CompanionApp.swift
+//  Travel Companion
+//
+//  Created by Justin Henley on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Travel_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Travel Companion/Travel CompanionTests/Travel_CompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_CompanionTests.swift
+//  Travel CompanionTests
+//
+//  Created by Justin Henley on 3/30/25.
+//
+
+import Testing
+@testable import Travel_Companion
+
+struct Travel_CompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
